196df8ed196eca1cb6f2aa9ab6f32765b770c83d
Juan Camilo <jcus98@gmail.com>
2026-05-30 16:42:46 -0500

    Remove cors() to let CorsHeaderFilter handle CORS

--- bora-backend/src/main/java/com/bora/borabackend/config/SecurityConfig.java
+++ bora-backend/src/main/java/com/bora/borabackend/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
