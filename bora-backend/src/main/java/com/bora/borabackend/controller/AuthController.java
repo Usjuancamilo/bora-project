989141e36fbf47df8dc1d6963e65b1a6613bf4a5
Juan Camilo <jcus98@gmail.com>
2026-05-30 15:28:24 -0500

    Update CORS config in AuthController with all methods

--- bora-backend/src/main/java/com/bora/borabackend/controller/AuthController.java
+++ bora-backend/src/main/java/com/bora/borabackend/controller/AuthController.java
@@ -1,25 +1,20 @@
 package com.bora.borabackend.controller;
 
-
 import org.springframework.web.bind.annotation.*;
-
-import javax.security.auth.login.LoginContext;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
-
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class AuthController {
 
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request){
-        //Spring security valida automaticamente con httpBasic
         return "{\"message\":\"Login exitoso\"}";
-
     }
 }
 
-    class LoginRequest {
-        public String username;
-        public String password;
-    }
+class LoginRequest {
+    public String username;
+    public String password;
+}
